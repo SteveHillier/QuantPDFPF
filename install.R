f78346036cfd5fcfd6a4ce44920c509e16b520ea
Stephen Hillier <Stephen.Hillier@hutton.ac.uk>
2025-11-02 19:57:31 +0000

    this is intial copy of course Notebook

---
+++ install.R
@@ -0,0 +1,10 @@
+install.packages("devtools")
+
+devtools::install_github('SteveHillier/powdR')
+# install.packages("powdR")
+install.packages("tictoc")
+install.packages("foreach")
+install.packages("doParallel")
+install.packages("tcltk")
+
+
